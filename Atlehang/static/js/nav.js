de24c1a9bbf772d87a7513f3d359471ce6ba5b9e
NTokozoMeto <ntokozometo@gmail.com>
2020-08-03 00:04:31 +0200

    autoprefixed

--- Atlehang/static/js/nav.js
+++ Atlehang/static/js/nav.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function ()
 {
     var nav_center_pos = window.getComputedStyle(document.querySelector('.nav')).height || document.querySelector('.nav').height;
@@ -6,3 +5,26 @@ $(document).ready(function ()
     $("#nav-btn-container").css({'top':0.5*parseInt(nav_center_pos,10)-0.25*parseInt(nav_height,10)});
 });
 
+
+document.getElementById('nav-btn-container').addEventListener('click',function(){
+    control_scroll();
+});
+
+
+function control_scroll(){
+    var check = document.getElementById('nav-btn-checkbox').checked;
+    if(check == true){
+        unfreeze();
+    }
+    else{
+        freeze();
+    }
+}
+function freeze(){
+    $('body').css({'overflow':'hidden'});
+    document.querySelector('.menu').scrollIntoView();    
+}
+function unfreeze(){
+    $('body').css({'overflow':'auto'});
+
+}
